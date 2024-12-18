84e67ad374698a86a7c8a18aace1a89def5ad9d3
Abdallah Mustapha <abdallah.mustapha@hubtel.com>
2024-12-18 21:49:40 +0000

    dusted dictionary

--- variable.py
+++ variable.py
@@ -1,10 +1,10 @@
 # # int, float, double
-# x = 1
-# y = 20
-# y = input("enter value to override <Y>")
-#
-# sum = x+ float(y)
-# print(f"value {sum}")
+x = 1
+y = 20
+y = input("enter value to override <Y>")
+
+sum = x+ float(y)
+print(f"value {sum}")
 
 
 # string
@@ -20,3 +20,17 @@ for split in other:
 print(f"full name {str(full_name)}")
 print(other)
 print(name)
+
+# dictionary
+person = {"name": "Mustapha", "age": 25, "country": "Ghana"}
+
+for key, value in person.items():
+    print(f"{key} : {value}")
+for key in person:
+    if "Age".lower().__eq__(key):
+        print("age key exist",key)
+        break
+    print(key)
+
+for value in person.values():
+    print(f"{value}")
