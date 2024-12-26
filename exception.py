bb3193d5f70e332a1bb88c22603f3d8be4bc539a
Abdallah Mustapha <abdallah.mustapha@hubtel.com>
2024-12-26 22:40:16 +0000

    added exception

--- exception.py
+++ exception.py
@@ -1,9 +1,9 @@
-def divide_num(x,y):
+def divide_num(numerator,denominator):
     divide = 0
     try:
-        divide = float(x)/ float(y)
+        divide = float(numerator)/ int(denominator)
 
-        divide = round(divide, 4)
+        divide = round(divide, 2)
         return divide
     except ZeroDivisionError:
         print("you can't divide by zero")
@@ -12,4 +12,25 @@ def divide_num(x,y):
         print(f"I'm staying here! {divide}")
 
 
-divide_num(2,1.1)
+#divide_num(2,2.2)
+
+def round_num():
+    try:
+        value = float(input("enter a number >> "))
+        value = round(value, 2)
+        return value
+
+    except ValueError as e:
+        print(f"please enter a number: {e}")
+        return None
+
+    except Exception as e:
+        print(f"any other exception happened::: {e}")
+        return None
+
+
+result = round_num()
+
+if result is None:
+    print("you didn't enter a number")
+print(f"result is {result}")
